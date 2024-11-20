cf85ce8dbaaa155cbcae1f25b9f2550c51c3eb22
Steffen Trumtrar <steffen@trumtrar.info>
2024-11-20 10:59:23 +0100

    flake: update
    Signed-off-by: Steffen Trumtrar <steffen@trumtrar.info>

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
           pname = "woodpecker-config-service";
           version = "v1.1.0";
           src = ./.;
-	  vendorHash = "sha256-cRXXMZ2KUp9A4HhiNrtClKP8FMnQ5YqKd/D3EOwPeSg=";
+	  vendorHash = "sha256-mvkfun1vsHyQwwzUS9c/Fc/XFYErIwnEX5vCGqOumZ0=";
 
 	  CGO_ENABLED = 1;
 
